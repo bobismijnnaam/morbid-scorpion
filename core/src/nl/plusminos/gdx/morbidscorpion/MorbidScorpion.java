93301785ffe85198b281850b92e24f6d5b065acc
Bob Rubbens <bobrubbens@gmail.com>
2014-04-20 09:23:25 +0200

    Inbetween

--- core/src/nl/plusminos/gdx/morbidscorpion/MorbidScorpion.java
+++ core/src/nl/plusminos/gdx/morbidscorpion/MorbidScorpion.java
@@ -6,8 +6,6 @@ import nl.plusminos.gdx.morbidscorpion.gamestates.Menu;
 import nl.plusminos.harness.gdx.gamestates.GamestateManager;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
